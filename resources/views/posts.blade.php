fd81bde3aa8251a9d379989d7ef5f160a18db8c2
Maria <masha.nichiporenko@gmail.com>
2022-06-19 18:46:42 +0300

    added posts and different pages for them

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">First Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non mauris vel augue ullamcorper egestas sed nec ex. Ut sodales sed neque eu interdum. Sed vitae nisl iaculis, facilisis ipsum a, interdum elit. Nunc tellus velit, dapibus sed erat vitae, facilisis aliquam nisl. Vivamus consectetur eget neque et varius. Proin sed nunc metus. Suspendisse vitae nisi orci. Duis dignissim condimentum urna, faucibus malesuada mi dictum quis. Donec ultrices nunc lorem, quis tincidunt massa vulputate consectetur. Nulla facilisi. Aliquam erat volutpat. Praesent et justo nec felis luctus rhoncus commodo sed arcu. Maecenas laoreet eget orci quis mattis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas sed efficitur elit, pulvinar ullamcorper ante.</p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-second-post">Second Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non mauris vel augue ullamcorper egestas sed nec ex. Ut sodales sed neque eu interdum. Sed vitae nisl iaculis, facilisis ipsum a, interdum elit. Nunc tellus velit, dapibus sed erat vitae, facilisis aliquam nisl. Vivamus consectetur eget neque et varius. Proin sed nunc metus. Suspendisse vitae nisi orci. Duis dignissim condimentum urna, faucibus malesuada mi dictum quis. Donec ultrices nunc lorem, quis tincidunt massa vulputate consectetur. Nulla facilisi. Aliquam erat volutpat. Praesent et justo nec felis luctus rhoncus commodo sed arcu. Maecenas laoreet eget orci quis mattis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas sed efficitur elit, pulvinar ullamcorper ante.</p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-third-post">Third Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non mauris vel augue ullamcorper egestas sed nec ex. Ut sodales sed neque eu interdum. Sed vitae nisl iaculis, facilisis ipsum a, interdum elit. Nunc tellus velit, dapibus sed erat vitae, facilisis aliquam nisl. Vivamus consectetur eget neque et varius. Proin sed nunc metus. Suspendisse vitae nisi orci. Duis dignissim condimentum urna, faucibus malesuada mi dictum quis. Donec ultrices nunc lorem, quis tincidunt massa vulputate consectetur. Nulla facilisi. Aliquam erat volutpat. Praesent et justo nec felis luctus rhoncus commodo sed arcu. Maecenas laoreet eget orci quis mattis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas sed efficitur elit, pulvinar ullamcorper ante.</p>
+    </article>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts', function () {
+    return view('posts');
+});
+
+Route::get('/posts/{post}', function ($slug) {
+    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+
+
+    if (!file_exists($path)) {
+        abort(404);
+    }
+
+    $post = file_get_contents($path);
+
+    return view('post', [
+        'post' => $post,
+    ]);
+});
