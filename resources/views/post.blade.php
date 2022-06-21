6881b2373a462baf09de33c66ef439fa63ffc4c9
Maria <masha.nichiporenko@gmail.com>
2022-06-21 14:30:51 +0300

    added layout-file, changed syntax

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,20 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-
-<body>
-    <article>
-        <h1>{{$post->title;}}</h1>
-        <p>{!! $post->body; !!}</p>
-    </article>
-    <p><a href="/posts">go back</a></p>
-</body>
-
-</html>
+@section('content')
+<article>
+    <h1>{{$post->title;}}</h1>
+    <p>{!! $post->body; !!}</p>
+</article>
+<p><a href="/posts">go back</a></p>
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,23 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-
-<body>
-    @foreach ($posts as $post)
-    <article>
-        <h1>
-            <a href="posts/{{ $post->slug; }}">{{ $post->title; }}</a>
-        </h1>
-        <p>{{ $post->excerpt; }}</p>
-    </article>
-    @endforeach
-</body>
-
-</html>
+@section('content')
+@foreach ($posts as $post)
+<article>
+    <h1>
+        <a href="posts/{{ $post->slug; }}">{{ $post->title; }}</a>
+    </h1>
+    <p>{{ $post->excerpt; }}</p>
+</article>
+@endforeach
+@endsection
