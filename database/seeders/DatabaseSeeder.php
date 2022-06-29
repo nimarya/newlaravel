a32f9b308c22a77a109a647a395167dad200e645
Maria <masha.nichiporenko@gmail.com>
2022-06-29 09:54:39 +0300

    made all table filled by seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,41 +21,6 @@ class DatabaseSeeder extends Seeder
         Category::truncate();
         Post::truncate();
 
-
-        $user = \App\Models\User::factory()->create();
-
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal',
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work',
-        ]);
-
-        $hobbies = Category::create([
-            'name' => 'Hobbies',
-            'slug' => 'hobbies',
-        ]);
-
-        Post::create([
-            'category_id' => $personal->id,
-            'user_id' => $user->id,
-            'slug' => 'my-first-post',
-            'title' => 'My First Post',
-            'excerpt' => 'Curabitur accumsan dapibus arcu.',
-            'body' => 'Curabitur accumsan dapibus arcu. Phasellus in vulputate tellus, ac aliquet nunc. Vivamus euismod efficitur eros, at rutrum purus commodo at. Praesent risus mi, porta at ullamcorper non, interdum nec dui. Donec lobortis nibh nec nisi feugiat, quis iaculis libero interdum. Mauris fringilla magna quis iaculis venenatis. Nunc convallis nisl augue, eu rhoncus justo placerat eget. Duis sagittis congue ex, eu luctus nisi vehicula a. Sed et sapien ex. Cras commodo lacus sed orci vestibulum ultrices. Aliquam efficitur est vel augue suscipit porta. Suspendisse ut faucibus nulla, eget convallis libero. Quisque condimentum justo nec eros tincidunt congue. Aliquam venenatis, risus eu pretium dictum, nisi urna bibendum dolor, in molestie elit mauris hendrerit leo.',
-        ]);
-
-        Post::create([
-            'category_id' => $work->id,
-            'user_id' => $user->id,
-            'slug' => 'my-second-post',
-            'title' => 'My Second Post',
-            'excerpt' => 'Curabitur accumsan dapibus arcu.',
-            'body' => 'Curabitur accumsan dapibus arcu. Phasellus in vulputate tellus, ac aliquet nunc. Vivamus euismod efficitur eros, at rutrum purus commodo at. Praesent risus mi, porta at ullamcorper non, interdum nec dui. Donec lobortis nibh nec nisi feugiat, quis iaculis libero interdum. Mauris fringilla magna quis iaculis venenatis. Nunc convallis nisl augue, eu rhoncus justo placerat eget. Duis sagittis congue ex, eu luctus nisi vehicula a. Sed et sapien ex. Cras commodo lacus sed orci vestibulum ultrices. Aliquam efficitur est vel augue suscipit porta. Suspendisse ut faucibus nulla, eget convallis libero. Quisque condimentum justo nec eros tincidunt congue. Aliquam venenatis, risus eu pretium dictum, nisi urna bibendum dolor, in molestie elit mauris hendrerit leo.',
-        ]);
+        Post::factory(3)->create();
     }
 }
