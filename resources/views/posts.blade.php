a61527d902c773918d5c0a7a2f84b61504641f54
Maria <masha.nichiporenko@gmail.com>
2022-06-29 21:37:15 +0300

    fixed multi-queries problem on category-posts page

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,7 +7,7 @@
         <a href="/posts/{{ $post->slug; }}">{{ $post->title; }}</a>
     </h1>
     <p>{{ $post->excerpt; }}</p>
-    <p>written by <a href="/authors/{{ $post->author->id; }}">{{ $post->author->name; }}</a> in <a href="/categories/{{ $post->category->slug; }}">{{ $post->category->name; }}</a></p>
+    <p>written by <a href="/authors/{{ $post->author->username; }}">{{ $post->author->name; }}</a> in <a href="/categories/{{ $post->category->slug; }}">{{ $post->category->name; }}</a></p>
 </article>
 @endforeach
 @endsection
