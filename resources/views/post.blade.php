2abb150159339a57eae0138e63304255578c1b1e
Maria <masha.nichiporenko@gmail.com>
2022-06-29 08:20:12 +0300

    made connections with user, added author to page

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,5 +5,6 @@
     <h1>{{$post->title;}}</h1>
     <p>{!! $post->body; !!}</p>
 </article>
+<p>written by <a href="/users/{{ $post->user->id; }}">{{ $post->user->name; }}</a> in <a href="/categories/{{ $post->category->slug; }}">{{ $post->category->name; }}</a></p>
 <p><a href="/posts">go to the main page</a></p>
 @endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,7 +7,7 @@
         <a href="/posts/{{ $post->slug; }}">{{ $post->title; }}</a>
     </h1>
     <p>{{ $post->excerpt; }}</p>
-    <p><a href="categories/{{ $post->category->slug; }}">{{ $post->category->name; }}</a></p>
+    <p>written by <a href="/users/{{ $post->user->id; }}">{{ $post->user->name; }}</a> in <a href="/categories/{{ $post->category->slug; }}">{{ $post->category->name; }}</a></p>
 </article>
 @endforeach
 @endsection
