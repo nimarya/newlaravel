730dcc125af2cd07f899513f9ac5042a262f7033
Maria <masha.nichiporenko@gmail.com>
2022-06-29 21:02:47 +0300

    fixed multi-queries for loading authors

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,6 +5,6 @@
     <h1>{{$post->title;}}</h1>
     <p>{!! $post->body; !!}</p>
 </article>
-<p>written by <a href="/users/{{ $post->user->id; }}">{{ $post->user->name; }}</a> in <a href="/categories/{{ $post->category->slug; }}">{{ $post->category->name; }}</a></p>
+<p>written by <a href="/users/{{ $post->author->id; }}">{{ $post->author->name; }}</a> in <a href="/categories/{{ $post->category->slug; }}">{{ $post->category->name; }}</a></p>
 <p><a href="/posts">go to the main page</a></p>
 @endsection
