b3382652b508c7348e44d64a629f5b51b2325d0d
Maria <masha.nichiporenko@gmail.com>
2022-07-01 21:02:32 +0300

    made dinamic posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,20 +4,15 @@
         @include ('_posts-header')
 
         <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+
             <x-post-featured-card :post="$posts[0]" />
 
-            <!-- <div class="lg:grid lg:grid-cols-2">
-                <x-post-card />
-                <x-post-card />
+            <div class="lg:grid lg:grid-cols-2">
+                @foreach ($posts->skip(1) as $post)
+                <x-post-card :post="$post" />
+                @endforeach
             </div>
 
-            <div class="lg:grid lg:grid-cols-3">
-                <x-post-card />
-
-                <x-post-card />
-
-                <x-post-card />
-            </div> -->
         </main>
     </x-slot>
 </x-layout>
