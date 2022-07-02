6a8396b086018d322ab5af92711a36b17a2bee4a
Maria <masha.nichiporenko@gmail.com>
2022-07-02 09:58:04 +0300

    added js component

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -14,17 +14,17 @@
         <!--  Category -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
 
-            <div>
+            <div x-data="{ show: false }">
                 <button>Categories</button>
 
-                <div>
+                <div x-show="show">
                     <a href="#">One</a>
                     <a href="#">Two</a>
                     <a href="#">Three</a>
                 </div>
             </div>
 
-
+{{-- 
             <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
 
                 <option value="category" disabled selected>Category</option>
@@ -33,7 +33,7 @@
                 <option value="{{ $category->slug; }}">{{ $category->name; }}</option>
                 @endforeach
 
-            </select>
+            </select> 
 
             <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22" height="22" viewBox="0 0 22 22">
                 <g fill="none" fill-rule="evenodd">
@@ -41,7 +41,7 @@
                     </path>
                     <path fill="#222" d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
                 </g>
-            </svg>
+            </svg> --}}
         </div>
 
         <!-- Other Filters -->
