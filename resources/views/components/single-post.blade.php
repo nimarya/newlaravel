d77106739d411174aac835c369a435583635acab
Maria <masha.nichiporenko@gmail.com>
2022-07-03 19:12:23 +0300

    made posts look prettier

--- resources/views/components/single-post.blade.php
+++ resources/views/components/single-post.blade.php
@@ -45,7 +45,7 @@
             </h1>
 
             <div class="space-y-4 lg:text-lg leading-loose">
-                <p>{{ $post->body; }}</p>
+                {!! $post->body; !!}
             </div>
         </div>
     </article>
