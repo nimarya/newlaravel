d9aef10a131d6d6ce601445e82a5f04deee4bee4
Maria <masha.nichiporenko@gmail.com>
2022-07-08 22:01:30 +0300

    incapculated category-dropdown to component with its own class

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,6 @@ class PostController extends Controller
     {
         return view('posts', [
             'posts' => Post::latest()->filter(request(['search', 'category']))->get(),
-            'categories' => Category::all(),
             'currentCategory' => Category::firstWhere('slug', request('category')),
         ]);
     }
--- routes/web.php
+++ routes/web.php
@@ -25,19 +25,9 @@ Route::get('/posts', [PostController::class, 'index']);
 
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
-// Route::get('/categories/{category:slug}', function (Category $category) {
-
-//     return view('posts', [
-//         'posts' => $category->posts,
-//         'categories' => Category::all(),
-//         'currentCategory' => $category,
-//     ]);
-// });
-
 Route::get('/authors/{author:username}', function (User $author) {
 
     return view('posts', [
         'posts' => $author->posts,
-        'categories' => Category::all(),
     ]);
 });
