c21cdcdc321871d414488928173cc99b8b0fc7cc
Maria <masha.nichiporenko@gmail.com>
2022-07-09 14:09:05 +0300

    fixed bug in filters

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -17,7 +17,7 @@
 
     @foreach ($categories as $category)
 
-        <x-dropdown-item href="?category={{ $category->slug; }}">
+        <x-dropdown-item href="?category={{ $category->slug; }}&{{ http_build_query(request()->except('category')); }}#">
             {{ ucwords($category->name); }}
         </x-dropdown-item>
 
