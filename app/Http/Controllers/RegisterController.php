8dbd8022d16ce2096694317721a5390643ea8ade
Maria <masha.nichiporenko@gmail.com>
2022-07-11 10:15:27 +0300

    added passwords hashing

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -13,11 +14,16 @@ class RegisterController extends Controller
 
     public function store()
     {
-        request()->validate([
+        $attributes = request()->validate([
             'name' => 'required|max:255',
             'username' => 'required|max:255',
             'email' => 'required|max:255|email',
             'password' => 'required|max:255|min:7',
         ]);
+
+        $attributes['password'] = bcrypt($attributes['password']);
+
+        User::create($attributes);
+        return redirect('/posts');
     }
 }
