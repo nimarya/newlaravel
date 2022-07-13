3ac2e19090072302fccb34862697b5aa16f20660
Maria <masha.nichiporenko@gmail.com>
2022-07-13 19:35:37 +0300

    created and styled a form for new comments

--- resources/views/components/single-post.blade.php
+++ resources/views/components/single-post.blade.php
@@ -50,6 +50,26 @@
         </div>
 
         <section class="col-span-8 col-start-5 mt-10 space-y-6">
+
+            <x-panel>
+                <form action="#" method="post">
+                    @csrf
+
+                    <header class="flex items-center">
+                        <img src="https://i.pravatar.cc/40" alt="" width="40" height="40" class="rounded-full">
+
+                        <h2 class="ml-4">Want to participate?</h2>
+                    </header>
+
+                    <div  class="mt-5">
+                        <textarea name="body" class="w-full text-sm focus:outline-none focus:ring" id=""  rows="5" placeholder="Leave your comment!"></textarea>
+                    </div>
+
+                    <div class="flex justify-end mt-5">
+                        <button type="submit" class="bg-blue-400 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
+                    </div>
+                </form>
+            </x-panel>
             @foreach ($post->comments as $comment)
                 <x-post-comment :comment="$comment"/>
             @endforeach
