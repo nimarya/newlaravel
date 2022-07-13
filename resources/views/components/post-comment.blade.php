0b1ec25c6c090ec6bf3aec8fa0bf0fbc0055fff4
Maria <masha.nichiporenko@gmail.com>
2022-07-13 18:07:57 +0300

    made comments dinamic

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,3 +1,5 @@
+@props(['comment'])
+
 <article class="flex bg-gray-100 border border-grat-200 p-6 rounded-xl space-x-4">
     <div class="flex-shrink-0">
         <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
@@ -5,19 +7,16 @@
 
     <div>
         <header class="mb-4">
-            <h3 class="font-bold">Maria Nichiporenko</h3>
+            <h3 class="font-bold">{{ $comment->author->username; }}</h3>
 
             <p class="text-xs">
                 Posted
-                <time>6 weeks ago</time>
+                <time>{{ $comment->created_at; }}</time>
             </p>
         </header>
 
         <p>
-            Perspiciatis rerum voluptas non facere praesentium
-             velit esse. Et quibusdam perferendis id. Ut iure aut autem et. 
-             Temporibus occaecati tempora optio iure fuga.
-              Aspernatur sint quibusdam unde voluptas.
+            {{ $comment->body; }}
         </p>
     </div>
 </article>
