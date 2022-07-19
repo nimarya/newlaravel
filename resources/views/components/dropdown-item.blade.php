22f1c090c60e1b52065a7ae31537faaba22c4cf7
Maria <masha.nichiporenko@gmail.com>
2022-07-19 16:35:14 +0300

    added dropdown to access create page, added marking of active link

--- resources/views/components/dropdown-item.blade.php
+++ resources/views/components/dropdown-item.blade.php
@@ -1,6 +1,8 @@
+@props(['active' => false])
 
 @php
     $classes = 'block text-left px-3 text-sm leading-6 hover:bg-gray-300 focus:bg-gray-300';
+    if ($active) $classes = $classes . ' bg-gray-300';
 @endphp
 
 <a {{ $attributes ([ 'class' => $classes]); }}>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -23,7 +23,15 @@
 
             <div class="mt-8 md:mt-0 flex items-center">
                 @auth
-                    <span class="text-xs font-bold uppercase">Welcome, {{ auth()->user()->name; }}!</span>
+                    <x-dropdown>
+                        <x-slot name="trigger">
+                            <button class="text-xs font-bold uppercase">Welcome, {{ auth()->user()->name; }}!</button>
+                        </x-slot>
+
+                        <x-dropdown-item href="/admin/posts/create" :active="request()->is('admin/posts/create')">
+                            New post
+                        </x-dropdown-item>
+                    </x-dropdown>
 
                     <form action="/logout" method="post" class="text-xs font-semibold text-blue-500 ml-4">
                         @csrf
